2a6f57e970d556c09dbd51d2419a30bae43191ac
iamrohitsampannavar <imrohitsampannavar@gmail.com>
2024-12-19 19:55:31 +0530

    navbar Component

--- job-portal/client/src/App.jsx
+++ job-portal/client/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Application from './pages/Application'
+import ApplyJob from './pages/ApplyJob'
 const App = () => {
   return (
     <div>
-
+<Routes>
+  <Route path='/' element={<Home/>} />
+  <Route path='/applications' element={<Application/>} />
+  <Route path='/apply-job/:id' element={<ApplyJob/>} />
+</Routes>
     </div>
   )
 }
