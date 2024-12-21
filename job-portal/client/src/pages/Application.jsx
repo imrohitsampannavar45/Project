cf0d5ef93b2742dc32742bbdb03c3ecaa4ffadc0
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-21 22:39:34 +0530

    halfway of application page

--- job-portal/client/src/pages/Application.jsx
+++ job-portal/client/src/pages/Application.jsx
@@ -16,11 +16,12 @@ const Application = () => {
         <div className='flex gap-2 mb-6 mt-3'>
             {isEdit 
             ? <>
-            <label htmlFor="resumeUpload">
-                <p>Select Resume</p>
-                <input id='resumeUpload' onChange={e => setResume(e.target.files[0])} accept="application/pdf" type='text' />
+            <label className='flex items-center' htmlFor="resumeUpload">
+                <p className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2'>Select Resume</p>
+                <input id='resumeUpload' onChange={e => setResume(e.target.files[0])} accept="application/pdf" type='file' hidden />
                 <img src={assets.profile_upload_icon} alt="" />
             </label>
+            <button className='bg-green-100 border border-green-400 rounded-lg px-4 py-2' >Save</button>
             
             </>:
             <div className='flex gap-2'>
