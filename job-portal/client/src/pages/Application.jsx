82ee4ac9f3ca7ee9202a8479dcf9fde81c312b88
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-21 22:24:03 +0530

    application page building

--- job-portal/client/src/pages/Application.jsx
+++ job-portal/client/src/pages/Application.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import Navbar from '../components/Navbar'   
+import { useState } from 'react'
 const Application = () => {
+
+    const [isEdit, setIsEdit] = useState(false);
+
     return (
       <>
       <Navbar />
+
+      <div className='container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10'>
+        <h2 className=''>Your Resume</h2>
+        <div>
+            {isEdit 
+            ? <>
+            
+            
+            </>:
+            <div>
+                <a href="">   Resume
+
+                </a>
+                <button>Edit</button>
+                 
+                </div>
+                }
+
+        </div>
+      </div>
       
       
       
