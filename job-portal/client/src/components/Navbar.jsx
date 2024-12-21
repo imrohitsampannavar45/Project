15f2d15bed873090f502c923eb71c4a7cfe78eef
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-21 21:47:31 +0530

    added styling nad usenavigate on navbar

--- job-portal/client/src/components/Navbar.jsx
+++ job-portal/client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {assets} from  '../assets/assets'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 const Navbar = () => {
 
 const {openSignIn} = useClerk()
 const {user} = useUser()
+const navigate = useNavigate()
 
 
 
@@ -13,7 +14,7 @@ const {user} = useUser()
   return (
     <div className='shadow py-4'>
       <div  className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
-        <img src={assets.logo} alt="" />
+        <img  onClick={()=>navigate('/')} className='cursor-pointer' src={assets.logo} alt="" />
         {
             user 
             ? <div className='flex items-center gap-3'>
--- job-portal/client/src/pages/ApplyJob.jsx
+++ job-portal/client/src/pages/ApplyJob.jsx
@@ -61,8 +61,15 @@ const ApplyJob = () => {
                             </div>
                         </div>
                         <div className='flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center'>
-                            <button className='bg-blue-600 p-2.5 px-10 text-white rounded '>Apply Now</button>
+                            <button className='bg-blue-600 p-2.5 px-10 text-white rounded'>Apply Now</button>
                             <p className='mt-1 text-gray-600'>Posted {moment(jobData.date).fromNow()}</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-col lg:flex-row justify-between items-start'>
+                        <div className='w-full lg:w-2/3'>
+                        <h2 className='font-bold text-2xl mb-4'>Job Description</h2>
+                        <div className='rich-text' dangerouslySetInnerHTML={{ __html: jobData.description }}></div>
+                        <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
                         </div>
                     </div>
                 </div>
@@ -70,7 +77,7 @@ const ApplyJob = () => {
         </>
     ) : (
         <Loading />
-    )
+    );
 }
 
 export default ApplyJob;
