d31870908a26b8b77db8b373972b9491871707dc
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-22 12:29:21 +0530

    Recruiter Login Page

--- job-portal/client/src/App.jsx
+++ job-portal/client/src/App.jsx
@@ -3,9 +3,11 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Application from './pages/Application'
 import ApplyJob from './pages/ApplyJob'
+import RecruiterLogin from './components/RecruiterLogin'
 const App = () => {
   return (
     <div>
+      <RecruiterLogin />
 <Routes>
   <Route path='/' element={<Home/>} />
   <Route path='/applications' element={<Application/>} />
---
+++ job-portal/client/src/components/RecruiterLogin.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const RecruiterLogin = () => {
+
+const [state, setState] = useState('Login');
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RecruiterLogin
