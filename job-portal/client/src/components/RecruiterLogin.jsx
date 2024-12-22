ff06401acf6c452179820ba52998d87b7bfd950e
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-22 12:31:04 +0530

    Login Page In Progress

--- job-portal/client/src/components/RecruiterLogin.jsx
+++ job-portal/client/src/components/RecruiterLogin.jsx
@@ -3,7 +3,11 @@ import React, { useState } from 'react'
 const RecruiterLogin = () => {
 
 const [state, setState] = useState('Login');
+const [name, setName] = useState('');
+const  [password ,setPassword] = useState('');
+const [email, setEmail] = useState(''); 
 
+const [image,setImage ] = useState(false);
 
   return (
     <div>
