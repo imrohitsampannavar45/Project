f0bf6697933b6cc28d0a23e32ca5d8f108ec8423
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-24 20:02:14 +0530

    Recruiter Login

--- job-portal/client/src/components/RecruiterLogin.jsx
+++ job-portal/client/src/components/RecruiterLogin.jsx
@@ -1,49 +1,94 @@
-import React, { useState } from 'react'
-import { assets } from '../assets/assets';
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const RecruiterLogin = () => {
+  const [state, setState] = useState("Login");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-const [state, setState] = useState('Login');
-const [name, setName] = useState('');
-const  [password ,setPassword] = useState('');
-const [email, setEmail] = useState(''); 
+  const [image, setImage] = useState(false);
 
-const [image,setImage ] = useState(false);
-
-const[isTextDataSubmitted, setIsTextDataSubmitted] = useState(false);
+  const [isTextDataSubmitted, setIsTextDataSubmitted] = useState(false);
 
   return (
-    <div>
-<form action="">
-    <h1>Recruiter {state}</h1>
-    <p>Welcome back ! Please Sign In to Continue..</p>
-<>
+    <div className="absolute top-0 left-0 right-0 bottom-0 backdrop-blur-sm bg-black/30 flex justify-center items-center">
+      <form className="relative bg-white p-10 rounded-lg flex flex-col items-center">
+        <h1 className="text-center text-2xl text-neutral-700 font-medium">
+          Recruiter {state}
+        </h1>
+        <p className="text-sm">Welcome back ! Please Sign In to Continue..</p>
+        <>
+          {state !== "Login" && (
+            <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+              <img src={assets.person_icon} alt="" />
+              <input
+                className="outline-none text-sm"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                type="text"
+                placeholder="Company Name"
+                required
+              />
+            </div>
+          )}
 
+          <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+            <img src={assets.email_icon} alt="" />
+            <input
+              className="outline-none text-sm"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              placeholder="Email ID"
+              required
+            />
+          </div>
 
-<div> 
-    <img src={assets.person_icon} alt="" />
-    <input onChange={e => setName(e.target.value)} value={name} type="text" placeholder='Company Name' required />
-</div>
+          <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+            <img src={assets.lock_icon} alt="" />
+            <input
+              className="outline-none text-sm"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <p className="text-sm text-blue-500 my-4 cursor-pointer">
+            Forgot Password ?
+          </p>
+        </>
 
+        <button className="bg-blue-600 w-full text-white py-2 rounded-full">
+          {state === "Login" ? "Login" : "Create Account"}
+        </button>
 
-
-<div> 
-    <img src={assets.email_icon} alt="" />
-    <input onChange={e => setEmail(e.target.value)} value={email} type="email" placeholder='Email ID' required />
-</div>
-
-<div> 
-    <img src={assets.lock_icon} alt="" />
-    <input onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder='Password' required />
-</div>
-
-
-
-</>
-
-</form>
+        {state === "Login" ? (
+          <p className="mt-5 text-center">
+            Don't have an Account ?{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Sign Up")}
+            >
+              Sign Up
+            </span>
+          </p>
+        ) : (
+          <p className="mt-5 text-center">
+            Aleredy have an Account ?{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Login")}
+            >
+              Login
+            </span>
+          </p>
+        )}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default RecruiterLogin
+export default RecruiterLogin;
