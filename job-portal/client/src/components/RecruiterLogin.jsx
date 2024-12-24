c6ae8395c9d38b10410c8846062bdd00e09300dd
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-24 20:05:39 +0530

    Recruiter Login Page

--- job-portal/client/src/components/RecruiterLogin.jsx
+++ job-portal/client/src/components/RecruiterLogin.jsx
@@ -18,48 +18,53 @@ const RecruiterLogin = () => {
           Recruiter {state}
         </h1>
         <p className="text-sm">Welcome back ! Please Sign In to Continue..</p>
-        <>
-          {state !== "Login" && (
+
+        {state === "Sign Up" && isTextDataSubmitted ? (
+          <></>
+        ) : (
+          <>
+            {state !== "Login" && (
+              <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+                <img src={assets.person_icon} alt="" />
+                <input
+                  className="outline-none text-sm"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  type="text"
+                  placeholder="Company Name"
+                  required
+                />
+              </div>
+            )}
+
             <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
-              <img src={assets.person_icon} alt="" />
+              <img src={assets.email_icon} alt="" />
               <input
                 className="outline-none text-sm"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                type="text"
-                placeholder="Company Name"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type="email"
+                placeholder="Email ID"
                 required
               />
             </div>
-          )}
 
-          <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
-            <img src={assets.email_icon} alt="" />
-            <input
-              className="outline-none text-sm"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              type="email"
-              placeholder="Email ID"
-              required
-            />
-          </div>
-
-          <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
-            <img src={assets.lock_icon} alt="" />
-            <input
-              className="outline-none text-sm"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              type="password"
-              placeholder="Password"
-              required
-            />
-          </div>
-          <p className="text-sm text-blue-500 my-4 cursor-pointer">
-            Forgot Password ?
-          </p>
-        </>
+            <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+              <img src={assets.lock_icon} alt="" />
+              <input
+                className="outline-none text-sm"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+          </>
+        )}
+        <p className="text-sm text-blue-500 my-4 cursor-pointer">
+          Forgot Password ?
+        </p>
 
         <button className="bg-blue-600 w-full text-white py-2 rounded-full">
           {state === "Login" ? "Login" : "Create Account"}
