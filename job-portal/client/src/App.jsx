82820e0248538fe8613c2d0c20102d88585e1837
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-12-29 20:50:13 +0530

    Dashboard Page

--- job-portal/client/src/App.jsx
+++ job-portal/client/src/App.jsx
@@ -1,25 +1,34 @@
-import React, { useContext } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Application from './pages/Application'
-import ApplyJob from './pages/ApplyJob'
-import RecruiterLogin from './components/RecruiterLogin'
-import { AppContext } from './context/AppContext'
-const App = () => {
+import React, { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Application from "./pages/Application";
+import ApplyJob from "./pages/ApplyJob";
+import RecruiterLogin from "./components/RecruiterLogin";
+import { AppContext } from "./context/AppContext";
+import Dashboard from "./pages/Dashboard";
+import ViewApplications from "./pages/ViewApplications";
+import ManageJobs from "./pages/ManageJobs";
+import AddJob from "./pages/AddJob";
 
-  const {showRecruiterLogin} = useContext(AppContext) 
+const App = () => {
+  const { showRecruiterLogin } = useContext(AppContext);
 
   return (
     <div>
-      {showRecruiterLogin && <RecruiterLogin />  }
- 
-<Routes>
-  <Route path='/' element={<Home/>} />
-  <Route path='/applications' element={<Application/>} />
-  <Route path='/apply-job/:id' element={<ApplyJob/>} />
-</Routes>
-    </div>
-  )
-}
+      {showRecruiterLogin && <RecruiterLogin />}
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/applications" element={<Application />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="view-applications" element={<ViewApplications />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
